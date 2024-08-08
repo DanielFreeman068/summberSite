be680ee05e2b8239a167e0128c6211919d6cebf5
DanielFreeman068 <141285622+DanielFreeman068@users.noreply.github.com>
2024-08-07 20:22:14 -0700

    update

--- index.js
+++ index.js
@@ -7,31 +7,3 @@ window.addEventListener('scroll', function() {
         header.style.backgroundColor = 'transparent';
     }
 });
-
-
-
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-showSlides(slideIndex = n);
-}
-
-//function to deal with 
-function showSlides(n) {
-let i;
-let slides = document.getElementsByClassName("mySlides");
-if (n > slides.length) {slideIndex = 1}
-if (n < 1) {slideIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}
-slides[slideIndex-1].style.display = "block";
-}
